10ab1e9c2dcb294f21eaa8fdbcea31e308f69f27
slavenr <ranisavljevicsan@gmail.com>
2022-03-27 12:01:42 +0200

    react markdown finished

--- react/simple-react-markdown/src/index.js
+++ react/simple-react-markdown/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App.jsx";
-import "./styles.scss"
+import "./styles.scss";
 
 ReactDOM.render(
     <Router>
